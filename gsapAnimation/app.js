d6748d84938566101d4bf82f1c94e73ee9839cad
TheNinza <niks.a3198@gmail.com>
2020-09-19 18:50:19 +0530

    Added gsapAnimation

---
+++ gsapAnimation/app.js
@@ -0,0 +1,47 @@
+const navButton = document.querySelector(".nav-button");
+const navOpen = document.querySelector(".nav-open");
+
+const tl = new TimelineLite({ paused: true, reversed: true });
+
+tl.to(".cover", 1, {
+  width: "60%",
+  ease: Power2.easeOut,
+})
+  .to(
+    "nav",
+    1,
+    {
+      height: "100%",
+      ease: Power2.easeOut,
+    },
+    "-= 0.5"
+  )
+  .fromTo(
+    ".nav-open",
+    0.5,
+    {
+      opacity: 0,
+      x: 50,
+      ease: Power2.easeOut,
+    },
+    {
+      opacity: 1,
+      x: 0,
+      onComplete: () => {
+        navOpen.style.pointerEvents = "auto";
+      },
+    }
+  );
+
+navButton.addEventListener("click", (e) => {
+  if (tl.isActive()) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    return false;
+  }
+  toggleTween(tl);
+});
+
+const toggleTween = (tween) => {
+  tween.reversed() ? tween.play() : tween.reverse();
+};
