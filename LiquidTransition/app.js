ddc0937cf32961933481d2f723aa05259cd168be
Nikhil Gupta <niks.a3198@gmail.com>
2020-09-24 22:17:01 +0530

    Added Liquify Hover

---
+++ LiquidTransition/app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.2,
+    image1: "./bw1.jpg",
+    image2: "./bw2.jpg",
+    displacementImage: "./dis.png",
+});
